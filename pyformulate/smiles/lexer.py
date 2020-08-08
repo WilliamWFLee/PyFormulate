d4837b98a0b03db3ff18d63823167916ee79b697
William Lee <wlee753159@gmail.com>
2020-08-08 21:55:23 +0100

    Add TokenName enum

--- pyformulate/smiles/lexer.py
+++ pyformulate/smiles/lexer.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from enum import Enum, auto
+
+
+class TokenName(Enum):
+    TERMINATOR = auto()
+    UCASE_ALPHA = auto()
+    LCASE_ALPHA = auto()
+    DIGIT = auto()
+    OPEN_PAREN = auto()
+    CLOSE_PAREN = auto()
+    OPEN_BRACK = auto()
+    CLOSE_BRACK = auto()
+    AT = auto()
+    DOT = auto()
+    HYPHEN = auto()
+    PLUS = auto()
+    EQUALS = auto()
+    HASH = auto()
+    DOLLAR = auto()
+    COLON = auto()
+    F_SLASH = auto()
+    B_SLASH = auto()
+    PERCENT = auto()
+    ASTERISK = auto()
+
 
 class Lexer:
     """
