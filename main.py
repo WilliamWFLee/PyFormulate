0305a95830958d39ee7997340b4496103836e2d7
William Lee <wlee753159@gmail.com>
2020-08-08 12:16:38 +0100

    Add argument parser

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("formula", help="The formula to parse")
+
+    return parser, parser.parse_args()
+
+
 def main():
-    pass
+    parser, args = parse_args()
 
 
 if __name__ == "__main__":
