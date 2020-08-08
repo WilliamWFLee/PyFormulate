a376f6a14b34f189ead0eafe72d63a4dd55a9cf9
William Lee <wlee753159@gmail.com>
2020-08-08 18:23:08 +0100

    Merge atom and bond files

--- pyformulate/smiles/models.py
+++ pyformulate/smiles/models.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from enum import Enum
-from typing import Optional, Union
+from typing import Optional, Tuple, Union, Sequence
 
 
 class Element(Enum):
@@ -156,3 +156,24 @@ class Atom:
         self.charge = charge
         self.chiral = chiral
         self.aromatic = aromatic
+
+
+class BondType(Enum):
+    """
+    Bond type enumeration
+    """
+
+    SINGLE = 1
+    DOUBLE = 2
+    TRIPLE = 3
+    QUADRUPLE = 4
+
+
+class Bond:
+    """
+    Represents a bond between two atoms
+    """
+
+    def __init__(self, atoms: Tuple[Atom, Atom], type_: BondType):
+        self.atoms = atoms
+        self.type = type_
