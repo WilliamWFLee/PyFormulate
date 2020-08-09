c0df5ed80f162fd6dd0e27d0eabb79f5a5948db9
William Lee <wlee753159@gmail.com>
2020-08-09 10:55:12 +0100

    Remove Symbol enum

--- pyformulate/smiles/parser.py
+++ pyformulate/smiles/parser.py
@@ -2,31 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from typing import Optional
-from enum import Enum, auto
-
-
-class Symbol(Enum):
-    TERMINATOR = auto()
-    UCASE_ALPHA = auto()
-    LCASE_ALPHA = auto()
-    DIGIT = auto()
-    OPEN_PAREN = auto()
-    CLOSE_PAREN = auto()
-    OPEN_BRACK = auto()
-    CLOSE_BRACK = auto()
-    AT = auto()
-    DOT = auto()
-    HYPHEN = auto()
-    PLUS = auto()
-    EQUALS = auto()
-    HASH = auto()
-    DOLLAR = auto()
-    COLON = auto()
-    F_SLASH = auto()
-    B_SLASH = auto()
-    PERCENT = auto()
-    ASTERISK = auto()
-    UNKNOWN = auto()
 
 
 class Parser:
