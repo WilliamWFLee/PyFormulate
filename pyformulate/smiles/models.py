c84bab3266f0f701a439e4d83021bc9274d00334
William Lee <wlee753159@gmail.com>
2020-08-10 19:36:16 +0100

    Add enumeration of the chiral classes

--- pyformulate/smiles/models.py
+++ pyformulate/smiles/models.py
@@ -145,6 +145,70 @@ class BondType(Enum):
     TOP_BOTTOM = 7
 
 
+class ChiralClass(Enum):
+    """
+    Enumeration for the tags used in SMILES for representing chiral stereochemistry
+    """
+
+    TH1 = 1
+    TH1 = 2
+    AL1 = 3
+    AL2 = 4
+    SP1 = 5
+    SP2 = 6
+    SP3 = 7
+    TB1 = 8
+    TB2 = 9
+    TB3 = 10
+    TB4 = 11
+    TB5 = 12
+    TB6 = 13
+    TB7 = 14
+    TB8 = 15
+    TB9 = 16
+    TB10 = 17
+    TB11 = 18
+    TB12 = 19
+    TB13 = 20
+    TB14 = 21
+    TB15 = 22
+    TB16 = 23
+    TB17 = 24
+    TB18 = 25
+    TB19 = 26
+    TB20 = 27
+    OH1 = 28
+    OH2 = 29
+    OH3 = 30
+    OH4 = 31
+    OH5 = 32
+    OH6 = 33
+    OH7 = 34
+    OH8 = 35
+    OH9 = 36
+    OH10 = 37
+    OH11 = 38
+    OH12 = 39
+    OH13 = 40
+    OH14 = 41
+    OH15 = 42
+    OH16 = 43
+    OH17 = 44
+    OH18 = 45
+    OH19 = 46
+    OH20 = 47
+    OH21 = 48
+    OH22 = 49
+    OH23 = 50
+    OH24 = 51
+    OH25 = 52
+    OH26 = 53
+    OH27 = 54
+    OH28 = 55
+    OH29 = 56
+    OH30 = 57
+
+
 class Atom:
     """
     Represents an atom, ion, etc.
