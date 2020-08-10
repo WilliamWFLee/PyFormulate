3c4875314b649279cbbc13f730eab479c70894d3
William Lee <wlee753159@gmail.com>
2020-08-10 14:06:00 +0100

    Change decoder to use PeekableStream

--- pyformulate/smiles/decoder.py
+++ pyformulate/smiles/decoder.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import re
 from typing import List, Optional, Tuple
 
 from .models import Atom, BondType, Molecule
 
-ALIPHATIC_ORGANIC_REGEX = re.compile(r"B|C|N|O|S|P|F|Cl|Br|I")
+ALIPHATIC_ORGANIC = ("B", "C", "N", "O", "S", "P", "F", "Cl", "Br", "I")
 AROMATIC_ORGANIC = "bcnosp"
 
 
@@ -67,39 +66,50 @@ class Decoder:
     def __init__(self, smiles: str):
         self.smiles = smiles
         self.species_list = []
+        self._stream = PeekableStream(smiles)
 
-    def _parse_aromatic_organic(self, start: int) -> Tuple[Atom, int]:
-        symbol = self.smiles[start]
+    def _parse_aromatic_organic(self) -> Atom:
+        symbol = next(self._stream)
         if symbol not in AROMATIC_ORGANIC:
-            raise DecodeError("Unknown organic aromatic element", self.smiles, start)
+            raise DecodeError(
+                "Unknown organic aromatic element", symbol, self._stream.pos - 1
+            )
         atom = Atom(symbol, aromatic=True)
-        return atom, start
+        return atom
 
-    def _parse_aliphatic_organic(self, start: int) -> Tuple[Atom, int]:
-        match = ALIPHATIC_ORGANIC_REGEX.match(self.smiles, start)
-        if not match:
-            raise DecodeError("Unknown organic aliphatic element", self.smiles, start)
+    def _parse_aliphatic_organic(self) -> Atom:
+        symbol = next(self._stream)
+        if symbol in ALIPHATIC_ORGANIC:
+            return Atom(symbol)
 
-        symbol = match.group()
-        atom = Atom(symbol)
-        return atom, match.end() - 1
+        next_char = self._stream.next
+        if not (next_char.isalpha() and next_char.islower()):
+            raise DecodeError(
+                "Unknown organic aliphatic element", symbol, self._stream.pos - 1
+            )
 
-    def _parse_organic_atom(self, start: int) -> Tuple[Atom, int]:
-        if self.smiles[start].islower():
-            atom, end = self._parse_aromatic_organic(start)
+        symbol += next(self._stream)
+        if symbol not in ALIPHATIC_ORGANIC:
+            raise DecodeError(
+                "Unknown organic aliphatic element", symbol, self._stream.pos - 2
+            )
+        return Atom(symbol)
+
+    def _parse_organic_atom(self) -> Atom:
+        if self._stream.next.islower():
+            atom = self._parse_aromatic_organic()
         else:
-            atom, end = self._parse_aliphatic_organic(start)
+            atom = self._parse_aliphatic_organic()
 
-        return atom, end
+        return atom
 
-    def _parse_branched_atom(self, start: int) -> Tuple[Optional[Atom], int]:
-        char = self.smiles[start : start + 1]
-        if char.isalpha():
-            atom, end = self._parse_organic_atom(start)
-            return atom, end
-        return None, start
+    def _parse_branched_atom(self) -> Optional[Atom]:
+        if self._stream.next.isalpha():
+            atom = self._parse_organic_atom()
+            return atom
+        return None
 
-    def _parse_bond(self, start: int) -> Tuple[BondType, int]:
+    def _parse_bond(self) -> Tuple[BondType]:
         char_to_bond_type = {
             "-": BondType.SINGLE,
             "=": BondType.DOUBLE,
@@ -110,16 +120,14 @@ class Decoder:
             "\\": BondType.TOP_BOTTOM,
         }
 
+        bond = next(self._stream)
         try:
-            return char_to_bond_type[self.smiles[start]], start
+            return char_to_bond_type[bond]
         except KeyError:
-            raise DecodeError("Unknown bond type", self.smiles, start)
+            raise DecodeError("Unknown bond type", bond, self._stream.pos - 1)
 
     def _parse_chain(
-        self,
-        start: int = 0,
-        molecule_idx: Optional[int] = None,
-        chain: Optional[List[Atom]] = None,
+        self, molecule_idx: Optional[int] = None, chain: Optional[List[Atom]] = None,
     ) -> Tuple[int, int]:
         if chain is None:
             chain = []
@@ -128,58 +136,57 @@ class Decoder:
             molecule_idx = len(self._molecules) - 1
 
         molecule = self._molecules[molecule_idx]
-
-        atom, idx = self._parse_branched_atom(start)
+        atom = self._parse_branched_atom()
         if atom:
             molecule.append(atom)
             chain.append(atom)
-            idx += 1
         else:
-            return molecule_idx, idx
+            return molecule_idx
 
-        char = self.smiles[idx : idx + 1]
-        if char:
-            if char in r"\/-=#$:":
-                bond_type, idx = self._parse_bond(idx)
-                idx += 1
+        char = self._stream.next
+        if not char:
+            return molecule_idx
+        if char in r"\/-=#$:":
+            bond_type = self._parse_bond()
 
-                length_before = len(chain)
-                molecule_idx, idx = self._parse_chain(idx, molecule_idx, chain)
-                length_after = len(chain)
+            length_before = len(chain)
+            molecule_idx = self._parse_chain(molecule_idx, chain)
+            length_after = len(chain)
 
-                if length_before == length_after:
-                    raise DecodeError(
-                        "Expected atom after bond symbol", self.smiles, idx - 1
-                    )
+            if length_before == length_after:
+                raise DecodeError(
+                    "Expected atom after bond symbol", char, self._stream.pos
+                )
 
+            last_atom = chain.pop()
+            chain[-1].bond(last_atom, bond_type)
+        elif char == ".":
+            next(self._stream)
+            molecule_idx = self._parse_chain()
+        else:
+            length_before = len(chain)
+            molecule_idx = self._parse_chain(molecule_idx, chain)
+            length_after = len(chain)
+
+            if length_before != length_after:
                 last_atom = chain.pop()
-                chain[-1].bond(last_atom, bond_type)
-            elif char == ".":
-                idx += 1
-                molecule_idx, idx = self._parse_chain(idx)
-            else:
-                length_before = len(chain)
-                molecule_idx, idx = self._parse_chain(idx, molecule_idx, chain)
-                length_after = len(chain)
+                chain[-1].bond(last_atom)
 
-                if length_before != length_after:
-                    last_atom = chain.pop()
-                    chain[-1].bond(last_atom)
-                    idx += 1
-
-        return molecule_idx, idx
+        return molecule_idx
 
     def decode(self) -> DecodeResult:
         self._molecules = []
-        idx = 0
+        if self._stream.next not in " \t\r\n":
+            self._parse_chain()
+            try:
+                terminator = next(self._stream)
+                if terminator not in " \t\r\n":
+                    raise DecodeError(
+                        "Unexpected character", terminator, self._stream.pos
+                    )
+            except StopIteration:
+                pass
 
-        if self.smiles[0] not in " \t\r\n":
-            _, idx = self._parse_chain(0)
-            char = self.smiles[idx : idx + 1]
-            if char not in " \t\r\n":
-                raise DecodeError("Unexpected character", self.smiles, idx)
-
-        remainder = self.smiles[idx + 2 :]
         return DecodeResult(
-            [Molecule(molecule) for molecule in self._molecules], remainder
+            [Molecule(molecule) for molecule in self._molecules], self._stream.remainder
         )
