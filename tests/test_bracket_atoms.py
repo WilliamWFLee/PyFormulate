c06100260514b05d2f30dceab377e6ce7e0aa7fd
William Lee <wlee753159@gmail.com>
2020-08-10 21:46:55 +0100

    Add hydrogen count tests

--- tests/test_bracket_atoms.py
+++ tests/test_bracket_atoms.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pytest
+
 from pyformulate.smiles import loads
+from pyformulate.smiles.decoder import DecodeError
 from pyformulate.smiles.models import Element
 
 
@@ -14,3 +17,17 @@ def test_known_elements():
 
 def test_unknown_element():
     assert loads("[*]").molecules[0].atoms[0].element == Element.UNKNOWN
+
+
+def test_explicit_hydrogen_count():
+    assert loads("[CH4]").molecules[0].elem_count(Element.H) == 4
+    assert loads("[ClH1]").molecules[0].elem_count(Element.H) == 1
+
+
+def test_implicit_hydrogen_count():
+    assert loads("[ClH]").molecules[0].elem_count(Element.H) == 1
+
+
+def test_illegal_hydrogen_count():
+    with pytest.raises(DecodeError):
+        loads("[HH1]")
