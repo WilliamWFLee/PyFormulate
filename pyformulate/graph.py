3d3b7530cdeb84aac3d5fb7b303e48f05420dfcb
William Lee <wlee753159@gmail.com>
2020-08-28 18:56:50 +0100

    Add Graph.new_node
    Also adds Graph.node_class class attribute

--- pyformulate/graph.py
+++ pyformulate/graph.py
@@ -11,6 +11,68 @@ from collections import defaultdict
 from typing import Any, Dict, Hashable, Optional
 
 
+class Node:
+    """
+    A class for representing a node on a graph.
+
+    Use of this class to represent the nodes
+    in an instance of :class:`InfoGraph` is completely optional,
+    however it can be useful to derive from this class,
+
+    .. attribute:: graph
+
+        The instance of :class:`InfoGraph` this node is associated with
+    """
+
+    def __init__(self, graph: Optional[InfoGraph] = None):
+        """
+        Instantiates an instance of a node, with an optional parameter
+        for the instance :class:`InfoGraph` it is associated to.
+        If it is not specified, a new graph is created for it.
+
+        :param graph: The graph, defaults to None
+        :type graph: Optional[InfoGraph]
+        """
+        if graph is None:
+            graph = InfoGraph()
+        self.graph = graph
+
+    def connect_to(self, other: Hashable, info: Any = None):
+        """
+        Connects this node to another node in the graph associated with this molecule.
+
+        Notice how the other value does not have to be an instance of :class:`Node`.
+
+        :param other: The value of the node to connect to
+        :type other: Hashable
+        :param info: The value associated with the new edge, defaults to None
+        :type info: Any
+        """
+        self.graph.connect(self, other, info)
+
+    def is_connected_to(self, other: Hashable) -> bool:
+        """
+        Determines if this node is connected to another node.
+
+        Like :meth:`Node.connect_to`, the other node value
+        does not have to be an instance of :class:`Node`.
+
+        :param other: The value of the node to check
+        :type other: Hashable
+        """
+        self.graph.are_connected(self, other)
+
+    def neighbours(self) -> Dict[Hashable, Any]:
+        """
+        Returns a dictionary of the neighbours of this node,
+        mapping the node to the info associated with the edge.
+
+        :return: The dictionary
+        :rtype: Dict[Hashable, Any]
+        """
+        return self.graph.neighbours_of(self)
+
+
 class InfoGraph:
     """
     A class for defining a graph whose edges have additional information
@@ -34,6 +96,8 @@ class InfoGraph:
     represents the graph with nodes `"A"` and `"B"`, with a single edge between them,
     having the associated value of `1`, representing a weight.
     """
+
+    node_class = Node
 
     def __init__(self):
         self._dict = defaultdict(dict)
@@ -95,70 +159,19 @@ class InfoGraph:
         except KeyError:
             raise ValueError(f"{value!r} is not in this graph")
 
+    def new_node(self) -> Node:
+        """
+        Creates a new node in this graph
+
+        :return: The newly created node
+        :rtype: Node
+        """
+        node = self.node_class(self)
+        self.add(node)
+        return node
+
     def __contains__(self, value):
         return value in self._dict
 
     def __iter__(self):
         return iter(self._dict)
-
-
-class Node:
-    """
-    A class for representing a node on a graph.
-
-    Use of this class to represent the nodes
-    in an instance of :class:`InfoGraph` is completely optional,
-    however it can be useful to derive from this class,
-
-    .. attribute:: graph
-
-        The instance of :class:`InfoGraph` this node is associated with
-    """
-
-    def __init__(self, graph: Optional[InfoGraph] = None):
-        """
-        Instantiates an instance of a node, with an optional parameter
-        for the instance :class:`InfoGraph` it is associated to.
-        If it is not specified, a new graph is created for it.
-
-        :param graph: The graph, defaults to None
-        :type graph: Optional[InfoGraph]
-        """
-        if graph is None:
-            graph = InfoGraph()
-        self.graph = graph
-
-    def connect_to(self, other: Hashable, info: Any = None):
-        """
-        Connects this node to another node in the graph associated with this molecule.
-
-        Notice how the other value does not have to be an instance of :class:`Node`.
-
-        :param other: The value of the node to connect to
-        :type other: Hashable
-        :param info: The value associated with the new edge, defaults to None
-        :type info: Any
-        """
-        self.graph.connect(self, other, info)
-
-    def is_connected_to(self, other: Hashable) -> bool:
-        """
-        Determines if this node is connected to another node.
-
-        Like :meth:`Node.connect_to`, the other node value
-        does not have to be an instance of :class:`Node`.
-
-        :param other: The value of the node to check
-        :type other: Hashable
-        """
-        self.graph.are_connected(self, other)
-
-    def neighbours(self) -> Dict[Hashable, Any]:
-        """
-        Returns a dictionary of the neighbours of this node,
-        mapping the node to the info associated with the edge.
-
-        :return: The dictionary
-        :rtype: Dict[Hashable, Any]
-        """
-        return self.graph.neighbours_of(self)
